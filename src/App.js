3ff6766d14e6a0b7b34ea76243ecb3429a3502ed
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-21 12:09:53 +0700

    init?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const base = "https://api.thecatapi.com/v1";
+
+  const [breeds, setbreeds] = useState([]);
+  const [cat, setCat] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const cat = await fetch(`${base}/breeds`);
+      const response = await cat.json();
+      setbreeds(response);
+      console.log(`${base}/breeds`);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const handleChange = async (e) => {
+    try {
+      const breedsId = e.target.value;
+      const breedsCat = await fetch(
+        `${base}/images/search?breed_ids=${breedsId}`
+      );
+      const response = await breedsCat.json();
+      setTimeout(() => {
+        setCat(response);
+      }, 500);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:max-w-2xl mx-auto px-5">
+      <select
+        className="w-full h-10 my-3 px-3 bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 backdrop-filter backdrop-saturate-150 backdrop-blur-lg rounded-md"
+        onChange={handleChange}
+      >
+        {breeds.map((items) => {
+          return (
+            <option key={items.id} value={items.id}>
+              {items.name}
+            </option>
+          );
+        })}
+      </select>
+      <div>
+        {cat.map((items) => {
+          const breeds = items.breeds[0];
+          return (
+            <div key={items.id} className="relative">
+              <img
+                className="object-cover w-full h-128 rounded-lg"
+                src={items.url}
+                alt={breeds.name}
+              />
+              <div className="absolute bottom-0 rounded-b-lg text-white p-6 space-y-4 bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 backdrop-filter backdrop-saturate-150 backdrop-blur-lg">
+                <h1 className="text-5xl font-bold font-mono">{breeds.name}</h1>
+                <p className="font-mono">breeds id : {breeds.id}</p>
+                <p className="font-mono">{breeds.description}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
